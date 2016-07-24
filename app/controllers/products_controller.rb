22b78080b4309c15c90f3fd5e56036e983ccc6df
Ana Minaya <analizetteminaya@gmail.com>
2016-07-24 09:36:48 -0400

    created routes and mapped to controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,60 @@
 class ProductsController < ApplicationController
-  def product
+
+  def index
     @products =Product.all
     #products is the controller I generated (always plural and lowercase)
     #Product is the model name which holds the attributes (always singular & 1st letter uppercase)
   end
+
+  def show
+    @products = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    @products = Product.new(
+    name:params[:name],
+    price:params[:price],
+    image:params[:image],
+    description:params[:description]
+    )
+    @products.save
+    render "create.html.erb"
+  end
+
+  def edit
+    @products = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @products = Product.find_by(id: params[:id])
+    @products.update(
+    name:params[:name],
+    price:params[:price],
+    image:params[:image],
+    description:params[:description]
+    )
+
+    render "update.html.erb"
+  end
+
+  def new_form
+    render "new_form.html.erb"
+  end
+
+  def new_product
+  Product.create(
+    name:params[:name],
+    price:params[:price],
+    image:params[:image],
+    description:params[:description]
+    )
+     render "new_form.html.erb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  get '/products' => 'products#product'
+  get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+  post '/products' => 'products#create'
+  get 'products/:id' => 'products#show'
+  get '/products/:id/edit'=> 'products#edit'
+  patch '/products/:id'=> 'products#update'
+  get '/form' => 'products#new_form'
+  post '/form' => 'products#new_product'
+
 end
